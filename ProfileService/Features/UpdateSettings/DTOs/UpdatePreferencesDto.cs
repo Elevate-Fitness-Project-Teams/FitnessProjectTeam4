d21a8417ef7b7c82f63aae8bfbb8dc2f27692ef2
Hazem Mofdi <hazemmofdi11@gmail.com>
2026-07-04 18:41:54 +0300

    Add DTOs and ViewModels for user settings updates
    Added `UpdateNotificationsDto`, `UpdatePreferencesDto`, and
    `UpdatePrivacyDto` in the `DTOs` namespace to represent
    notification, preference, and privacy settings, respectively.
    Introduced `UpdateSettingsDto` to encapsulate these DTOs for
    a unified settings update structure.
    
    Added corresponding `ViewModel` classes:
    `UpdateNotificationsViewModel`, `UpdatePreferencesViewModel`,
    and `UpdatePrivacyViewModel` in the `ViewModels` namespace,
    mirroring the DTOs. Introduced `UpdateSettingsRequestViewModel`
    to encapsulate these ViewModels for a unified structure in the
    view layer.

---
+++ ProfileService/Features/UpdateSettings/DTOs/UpdatePreferencesDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ProfileService.Features.UpdateSettings.DTOs
+{
+    public class UpdatePreferencesDto
+    {
+        public string? Language { get; set; }
+        public string? Theme { get; set; }
+        public string? WeightUnit { get; set; }
+        public string? HeightUnit { get; set; }
+        public string? DistanceUnit { get; set; }
+    }
+}
